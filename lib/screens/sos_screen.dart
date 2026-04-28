e16081d68bea6510a4917d46978504d3e180e548
Mahi Jain <cse.mahi235@gmail.com>
2026-04-28 18:20:24 +0530

    Update sos_screen.dart

--- lib/screens/sos_screen.dart
+++ lib/screens/sos_screen.dart
@@ -1,4 +1,23 @@
+// import 'package:flutter/material.dart';
+
+// class SOSScreen extends StatelessWidget {
+//   const SOSScreen({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Colors.black,
+//       body: const Center(
+//         child: Text(
+//           "SOS Screen",
+//           style: TextStyle(color: Colors.white, fontSize: 24),
+//         ),
+//       ),
+//     );
+//   }
+// }
 import 'package:flutter/material.dart';
+import 'countdown_screen.dart';
 
 class SOSScreen extends StatelessWidget {
   const SOSScreen({super.key});
@@ -6,11 +25,61 @@ class SOSScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      body: const Center(
-        child: Text(
-          "SOS Screen",
-          style: TextStyle(color: Colors.white, fontSize: 24),
+      backgroundColor: Colors.red.shade900,
+      appBar: AppBar(
+        title: const Text("Emergency SOS"),
+        backgroundColor: Colors.red,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "Are you in danger?",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+
+            const SizedBox(height: 30),
+
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.white,
+                foregroundColor: Colors.red,
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 60,
+                  vertical: 20,
+                ),
+              ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) => const CountdownScreen(),
+                  ),
+                );
+              },
+              child: const Text(
+                "START SOS",
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+
+            const SizedBox(height: 20),
+
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text(
+                "Cancel",
+                style: TextStyle(color: Colors.white),
+              ),
+            )
+          ],
         ),
       ),
     );
